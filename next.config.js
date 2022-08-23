789c8be9a87349a321e97fa46b9394e7757d5dab
killbizz <45452791+killbizz@users.noreply.github.com>
2022-08-23 12:54:53 +0200

    added some project configurations

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+// next.config.js
+
+const securityHeaders = [
+  {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block'
+  },
+  {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff'
+  }   
+];
+
 module.exports = {
-  reactStrictMode: true,
+async headers() {
+  return [
+    {
+      // Apply these headers to all routes in your application.
+      source: '/:path*',
+      headers: securityHeaders,
+    },
+  ]
+},
 }
