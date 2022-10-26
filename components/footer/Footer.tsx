8f965175771fa823278db746624b0d2c26670a01
killbizz <45452791+killbizz@users.noreply.github.com>
2022-10-27 00:30:38 +0200

    updated the layout

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,16 +6,11 @@ const Footer = () => {
                     <div className="col-6">
                         <span className="copyright" >&copy; 2022 Gabriel Bizzo</span>
                     </div>
-                    <div className="col-3">
-                        <a href="https://www.facebook.com/profile.php?id=100009326235389">
-                            <i className="fa fa-facebook fa-2x github-logo copyright" />
+                    <div className="col-6">
+                        <a href="https://github.com/killbizz/Online-Voting-React-FE">
+                            <i className="fa fa-github fa-3x github-logo copyright" />
                         </a>
-                    </div>
-                    <div className="col-3">
-                        <a href="https://www.instagram.com/claudiobizzo/">
-                            <i className="fa fa-instagram fa-2x github-logo copyright" />
-                        </a>
-                    </div>   
+                    </div>    
                 </div>
             </div>
         </footer>
--- components/navbar/NavigationBar.tsx
+++ components/navbar/NavigationBar.tsx
@@ -11,38 +11,52 @@ const NavigationBar = () => {
         <>
             <Navbar className="color-nav" collapseOnSelect expand="lg" variant="light">
                 <Container>
-                <Navbar.Brand>
-                    <Link href="/">
-                        <a className="nav-link active" aria-current="page">
-                            <Image id="VotingOnlineWebApplication" src="/voting-box.png" alt="Online Voting Web Application Logo" width="60" height="60" />
-                        </a>
-                    </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse className="justify-content-center pr-5 mr-5" id="responsive-navbar-nav">
-                    <Nav className="mx-auto">
-                        <Nav.Item className=" mx-4">
-                            <Link href="/">
-                                <a className="nav-link">Homepage</a>
-                            </Link>
-                        </Nav.Item>
-                        <Nav.Item className=" mx-4">
-                            <Link href="/user-dashboard">
-                                <a className="nav-link">Galleria</a>
-                            </Link>
-                        </Nav.Item>
-                        <Nav.Item className=" mx-4">
-                            <Link href="/admin-dashboard">
-                                <a className="nav-link">Chi Sono</a>
-                            </Link>
-                        </Nav.Item>
-                        <Nav.Item className=" mx-4">
-                            <Link href="/admin-dashboard">
-                                <a className="nav-link">Contattami</a>
-                            </Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
+                    <Navbar.Brand>
+                        <Link href="/">
+                            <a className="nav-link active" aria-current="page">
+                                <Image id="VotingOnlineWebApplication" src="/voting-box.png" alt="Online Voting Web Application Logo" width="60" height="60" />
+                            </a>
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Brand>
+                        <div>
+                            <a href="https://www.facebook.com/profile.php?id=100009326235389">
+                                <i className="fa fa-facebook-square fa-2x github-logo copyright" />
+                            </a>
+                        </div>  
+                    </Navbar.Brand>
+                    <Navbar.Brand>
+                        <div>
+                            <a href="https://www.instagram.com/claudiobizzo/">
+                                <i className="fa fa-instagram fa-2x github-logo copyright" />
+                            </a>
+                        </div>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-center" id="responsive-navbar-nav">
+                        <Nav className="ml-auto d-flex justify-content-end">
+                            <Nav.Item className=" mx-4">
+                                <Link href="/">
+                                    <a className="nav-link">Homepage</a>
+                                </Link>
+                            </Nav.Item>
+                            <Nav.Item className=" mx-4">
+                                <Link href="/gallery">
+                                    <a className="nav-link">Galleria</a>
+                                </Link>
+                            </Nav.Item>
+                            <Nav.Item className=" mx-4">
+                                <Link href="/about">
+                                    <a className="nav-link">Chi Sono</a>
+                                </Link>
+                            </Nav.Item>
+                            <Nav.Item className=" mx-4">
+                                <Link href="/contact">
+                                    <a className="nav-link">Contattami</a>
+                                </Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
