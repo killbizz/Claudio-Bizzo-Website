6dd0c08d7aa69d1d43f7f01bca55fa9b27f8b75a
killbizz <45452791+killbizz@users.noreply.github.com>
2022-11-09 17:37:37 +0100

    configured cloudinary hostname under nextjs config

--- next.config.js
+++ next.config.js
@@ -12,13 +12,17 @@ const securityHeaders = [
 ];
 
 module.exports = {
-async headers() {
-  return [
-    {
-      // Apply these headers to all routes in your application.
-      source: '/:path*',
-      headers: securityHeaders,
-    },
-  ]
-},
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+  reactStrictMode: true, 
+  images: {
+    domains: ['res.cloudinary.com'],
+  }
 }
