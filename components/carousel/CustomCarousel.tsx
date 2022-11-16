c04d41087b531f76dca3a9eafe6eeecc716eab4b
killbizz <45452791+killbizz@users.noreply.github.com>
2022-11-16 18:18:40 +0100

    added a handler function for the onClickItem event

--- components/carousel/CustomCarousel.tsx
+++ components/carousel/CustomCarousel.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from 'react-responsive-carousel';
 import { Artwork } from '../../types/Artwork';
 import Image from "next/image";
+import Router  from 'next/router';
 
 type Props = {
     artworks: Artwork[]
@@ -13,18 +14,18 @@ type Props = {
       <div id='carousel_cards_wrapper'>
         <Carousel showArrows={true} showStatus={false} showThumbs={false} interval={3500} transitionTime={1500} 
           autoPlay={autoplay} infiniteLoop useKeyboardArrows
-          onClickItem={(index, item) => {
+          onClickItem={(index, item: any) => {
             if(!handleOnClickItem)
               return;
-            // TODO : redirect to the item page
-            console.log(item);
-            // Router.push('/gallery');
+            // redirect to the item page
+            // key == publicId == 'soli-di-claudio/folder/filename'
+            Router.push('/artwork/' + item.key.split("/")[1]);
           }}
         >
           {
             artworks.map((artwork) => 
               <div key={artwork.publicId} className='card-flyer image-box'>
-                <Image src={artwork.imageURL} alt={artwork.title} layout='fill' objectFit='cover' />
+                <Image src={artwork.imageURL} id={artwork.publicId} alt={artwork.title} layout='fill' objectFit='cover' />
                 <p className="legend">{artwork.title}</p>
               </div>
             )
