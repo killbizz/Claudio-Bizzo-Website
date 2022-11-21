88c06f3c8364b52d93382baa74d431e6f84478cf
killbizz <45452791+killbizz@users.noreply.github.com>
2022-11-21 17:56:46 +0100

    started adding the artwork page right part

--- pages/artwork/[id].tsx
+++ pages/artwork/[id].tsx
@@ -13,14 +13,14 @@ const ArtworkPage = ({ artworkItems } : ArtworkPageProps) => {
 
   console.log(artworkItems);
 
-  const title: string = artworkItems.filter((value) => value.title !== undefined || value.title !== null)[0].title;
+  const artwork = artworkItems.filter((value) => value.title !== undefined || value.title !== null)[0];
 
   return(
-      <Layout title = {`I Soli di Claudio | ${title}`}>
+      <Layout title = {`I Soli di Claudio | ${artwork.title}`}>
         <div className="mid mid-background-color">
           <div className="container-fluid h-100">
             <div className="row h-100">
-              <div className='col-xl-5 border border-primary artwork-page-container h-100'>
+              <div className='col-xl-5 artwork-page-container h-100'>
                 <div className="artwork-page-mid">
                   <div className='artwork-title-container'>
                     <h2 className='artwork-title my-auto'>DAGHENEEEE EEEEEEEE eeeee eeeeee eeee EEEEEE EEE EEE</h2>
@@ -28,14 +28,60 @@ const ArtworkPage = ({ artworkItems } : ArtworkPageProps) => {
                   <CustomCarousel artworks={artworkItems} autoplay={false} handleOnClickItem={false} />
                 </div>
               </div>
-              <div className='col border border-primary artwork-page-container'>
-                <p>DAGHE</p>
-                <p>DAGHE</p>
-                <p>DAGHE</p>
-                <p>DAGHE</p>
-                <p>DAGHE</p>
-                <p>DAGHE</p>
-                <p>DAGHE</p>
+              <div className='col artwork-page-container artwork-right-section-background'>
+                <div className='artwork-right-section-container'>
+                  <div className='artwork-features-container mt-4 pt-2'>
+                    <div className='row justify-content-center'>
+                      <div className='col col-sm-auto'>
+                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col'>
+                        <p>Disponibile</p>
+                      </div>
+                    </div>
+
+                    <div className='row justify-content-center'>
+                      <div className='col col-sm-auto'>
+                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col'>
+                        <p>Legno, oro 24K, bronzo, zefiro, corda di bambù</p>
+                      </div>
+                    </div>
+
+                    <div className='row justify-content-center'>
+                      <div className='col col-sm-auto'>
+                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col'>
+                        <p>Dimensioni: 150x150x10 cm (Larghezza/Altezza/Profondità)</p>
+                      </div>
+                    </div>
+
+                    <div className='row justify-content-center'>
+                      <div className='col col-sm-auto'>
+                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col'>
+                        <p>Prodotto puramente con materiali eco-sostenibili</p>
+                      </div>
+                    </div>
+
+                    <div className='row justify-content-center'>
+                      <div className='col col-sm-auto'>
+                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col'>
+                        <p>Materiali e manodopera Italiana</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='artwork-description-container mt-4 pt-2'>
+                    <div>
+                      {artwork.description}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -52,8 +98,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const paths = folders.map((folder) => ({
     params: { id: folder.name },
   }))
-
-  console.log(paths);
 
   // pre-render only these paths at build time.
   // { fallback: false } => other routes should 404.
