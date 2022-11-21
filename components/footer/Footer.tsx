0a1e3a8d0b89c846b660f77d8ff7b3e9f9575d26
killbizz <45452791+killbizz@users.noreply.github.com>
2022-11-21 17:57:23 +0100

    updated the footer removing the github link, leaved only a copyright notice with a link

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,16 +1,13 @@
 const Footer = () => {
     return (
         <footer className="footer mt-auto">
-            <div className="container text-center">
+            <div className="container-footer container text-center">
                 <div className="row justify-content-center">
-                    <div className="col-6">
-                        <span className="copyright" >&copy; 2022 Gabriel Bizzo</span>
-                    </div>
-                    <div className="col-6">
-                        <a href="https://github.com/killbizz/Online-Voting-React-FE">
-                            <i className="fa fa-github fa-3x logos copyright" />
+                    <div className="col px-0">
+                        <a href="https://www.linkedin.com/in/gabriel-bizzo-6b332b231/" className="simpleLink">
+                            <span className="copyright" >&copy; 2022 Gabriel Bizzo</span>
                         </a>
-                    </div>    
+                    </div>  
                 </div>
             </div>
         </footer>
