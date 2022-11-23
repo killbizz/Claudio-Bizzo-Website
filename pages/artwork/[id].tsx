2eb83c86b28f634b8ccf40152e416aeb850d5374
killbizz <45452791+killbizz@users.noreply.github.com>
2022-11-23 18:09:02 +0100

    added the get-info button and updated the style for the artwork page

--- pages/artwork/[id].tsx
+++ pages/artwork/[id].tsx
@@ -4,14 +4,13 @@ import Layout from '../../components/Layout';
 import { getArtworkInFolder, getFolder } from '../../services/artwork';
 import { Artwork } from '../../types/Artwork';
 import Folder from '../../types/Folder';
+import Router  from 'next/router';
 
 interface ArtworkPageProps {
     artwork: Artwork
 }
 
 const ArtworkPage = ({ artwork } : ArtworkPageProps) => {
-
-  // console.log(artwork);
 
   return(
       <Layout title = {`I Soli di Claudio | ${artwork.data.title}`}>
@@ -21,60 +20,87 @@ const ArtworkPage = ({ artwork } : ArtworkPageProps) => {
               <div className='col-xl-5 artwork-page-container h-100'>
                 <div className="artwork-page-mid">
                   <div className='artwork-title-container'>
-                    <h2 className='artwork-title my-auto'>DAGHENEEEE EEEEEEEE eeeee eeeeee eeee EEEEEE EEE EEE</h2>
+                    <h2 className='my-auto'>{artwork.data.title}</h2>
                   </div>
                   <CustomCarousel artworks={Array<Artwork>().concat(artwork)} autoplay={false} handleOnClickItem={false} />
+                  <div className='artwork-get-info-container row justify-content-center align-items-center'>
+                    <p className='col text-center mb-0 pt-3'>Sei interessato?</p>
+                    <div className='w-100' />
+                    <button 
+                      className="btn btn-lg custom-button artwork-get-info-btn mx-auto text-center d-block col"
+                      onClick={() => Router.push(`/contact?reason=${artwork.imageFiles[0].publicId.split("/")[1]}`)}
+                    >
+                      Chiedi informazioni
+                    </button>
+                  </div>
                 </div>
               </div>
               <div className='col artwork-page-container artwork-right-section-background'>
                 <div className='artwork-right-section-container'>
                   <div className='artwork-features-container mt-4 pt-2'>
+
                     <div className='row justify-content-center'>
-                      <div className='col col-sm-auto'>
-                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                    <div className='col bigScreen col-sm-auto'>
+                        <i className="fa fa-info-circle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col smallScreen col-3'>
+                        <i className="fa fa-info-circle fa-2x artwork-feature-icon" />
                       </div>
                       <div className='col'>
-                        <p>Disponibile</p>
+                        <p>{artwork.data.availability}</p>
                       </div>
                     </div>
 
                     <div className='row justify-content-center'>
-                      <div className='col col-sm-auto'>
-                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      <div className='col bigScreen col-sm-auto'>
+                        <i className="fa fa-info-circle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col smallScreen col-3'>
+                        <i className="fa fa-info-circle fa-2x artwork-feature-icon" />
                       </div>
                       <div className='col'>
-                        <p>Legno, oro 24K, bronzo, zefiro, corda di bambù</p>
+                        <p>{artwork.data.materials}</p>
                       </div>
                     </div>
 
                     <div className='row justify-content-center'>
-                      <div className='col col-sm-auto'>
-                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      <div className='col bigScreen col-sm-auto'>
+                        <i className="fa fa-arrows-alt fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col smallScreen col-3'>
+                        <i className="fa fa-arrows-alt fa-2x artwork-feature-icon" />
                       </div>
                       <div className='col'>
-                        <p>Dimensioni: 150x150x10 cm (Larghezza/Altezza/Profondità)</p>
+                        <p>{artwork.data.dimensions}</p>
                       </div>
                     </div>
 
                     <div className='row justify-content-center'>
-                      <div className='col col-sm-auto'>
-                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      <div className='col bigScreen col-sm-auto' style={{"paddingLeft": "11px"}}>
+                        <i className="fa fa-leaf fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col smallScreen col-3'>
+                        <i className="fa fa-leaf fa-2x artwork-feature-icon" />
                       </div>
                       <div className='col'>
-                        <p>Prodotto puramente con materiali eco-sostenibili</p>
+                        <p>Manodopera eco-sostenibile</p>
                       </div>
                     </div>
 
                     <div className='row justify-content-center'>
-                      <div className='col col-sm-auto'>
-                        <i className="fa fa-check-circle fa-2x artwork-feature-icon" />
+                      <div className='col bigScreen col-sm-auto' style={{"paddingLeft": "11px"}}>
+                        <i className="fa fa-recycle fa-2x artwork-feature-icon" />
+                      </div>
+                      <div className='col smallScreen col-3'>
+                        <i className="fa fa-recycle fa-2x artwork-feature-icon" />
                       </div>
                       <div className='col'>
-                        <p>Materiali e manodopera Italiana</p>
+                        <p>Utilizzo di materiali riciclati</p>
                       </div>
                     </div>
+
                   </div>
-                  <div className='artwork-description-container mt-4 pt-2'>
+                  <div className='artwork-description-container my-4 pt-2'>
                     <div>
                       {artwork.data.description}
                     </div>
