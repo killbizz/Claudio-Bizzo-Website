4e4fdb971affc7c890c5c94ddf7d2443a3402438
Gabriel Bizzo <gabriel.bizzo@hotmail.it>
2022-11-30 18:06:10 +0100

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Router from "next/router";
+import Layout from "../components/Layout"
+
+const CustomError404 = () => {
+  return (
+    <>
+      <Layout title="Pagina non trovata | I Soli di Claudio">
+        <div className="error-page-mid mid-background-color justify-content-center">
+            <div className="container col-xxl-8">
+                <div className="row flex-lg-row-reverse align-items-center g-5">
+                    <div className="col-10 col-sm-8 col-lg-5 justify-content-center my-auto mx-auto pt-5">
+                        <img src="/500_mascotte.png" className="d-block img-fluid ml-4" alt="500 Internal Server Error" width="1200" height="900" loading="lazy" />
+                    </div>
+                    <div className="col-lg-6">
+                        <h1 className="display-5 text-center fw-bold lh-1 mb-5">Pagina non trovata</h1>
+                        <p className="lead">Potresti aver utilizzato un link corrotto o che non esiste in questo sito, per favore controlla di aver digitato un URL corretto.</p>
+                        <p className="lead">
+                          In caso di problemi per favore
+                          <Link legacyBehavior href="/contact?reason=errore-sito"> 
+                            <a className="custom-darkblue-link"> contattami </a>
+                          </Link>
+                          spiegando il problema che hai incontrato per permettermi di migliorare il servizio.
+                        </p>
+                        <div className="d-grid gap-3 d-md-flex justify-content-md-start my-5 py-2">
+                            <button 
+                                className="btn btn-lg custom-button custom-button-dark-secondary my-auto mx-auto text-center d-block"
+                                onClick={() => Router.push('/')}
+                            >
+                                Torna alla Homepage
+                            </button>
+                            <button 
+                                className="btn btn-lg custom-button custom-button-dark my-auto mx-auto text-center d-block"
+                                onClick={() => Router.push('/gallery')}
+                            >
+                                Esplora la Galleria
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default CustomError404;
