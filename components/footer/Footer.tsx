98ffd42137198781ebe9e2f2c743a9588b006dca
killbizz <45452791+killbizz@users.noreply.github.com>
2022-12-06 13:36:56 +0100

    Update Footer.tsx

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,12 +2,18 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto">
             <div className="container-footer container text-center">
-                <div className="row justify-content-center">
-                    <div className="col px-0">
-                        <a href="https://www.linkedin.com/in/gabriel-bizzo-6b332b231/" className="simpleLink">
-                            <span className="copyright" >&copy; 2022 Gabriel Bizzo</span>
-                        </a>
-                    </div>  
+                <div className="row justify-content-center align-items-center h-100">
+                    <div className="col-sm-6">
+                        <span className="copyright" >&copy; 2022 Claudio Bizzo</span>
+                    </div>
+                    <div className="col-sm-6">
+                        <span className="copyright" >
+                            Powered by&nbsp;
+                            <a href="https://www.linkedin.com/in/gabriel-bizzo-6b332b231/" className="simpleLink">
+                                Gabriel Bizzo
+                            </a>
+                        </span>
+                    </div>
                 </div>
             </div>
         </footer>
