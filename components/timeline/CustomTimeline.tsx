7e347f33fe32987454d2c32e9c1ceae0e57e1f44
killbizz <45452791+killbizz@users.noreply.github.com>
2022-12-07 19:24:37 +0100

    now if there are no future events the section is not rendered

--- components/timeline/CustomTimeline.tsx
+++ components/timeline/CustomTimeline.tsx
@@ -13,58 +13,58 @@ type Props = {
 const CustomTimeline = ({ events }: Props) => {
   const today: Date = new Date();
 
+  const isEventDateAfterToday = (value: Event) => {
+    const dateParts: string[] = value.data_fine.split("/");
+    const date = new Date();
+    date.setFullYear(+dateParts[0], +dateParts[1] - 1, +dateParts[2]);
+    date.setUTCHours(21, 59, 0, 0);
+    return date >= today;
+  };
+
   return (
     <>
-      <h3 className="homepage-subtitle text-light text-center my-5">
-        Dove possiamo incontrarci
-      </h3>
-      <VerticalTimeline
-        className="vertical-timeline-custom-line mt-2"
-      >
-        {events
-          .filter((value) => {
-            const dateParts: string[] = value.data_fine.split("/");
-            const date = new Date();
-            date.setFullYear(+dateParts[0], +dateParts[1] - 1, +dateParts[2]);
-            date.setUTCHours(21, 59, 0, 0);
-            return date >= today;
-          })
-          .map((value) => (
-            <Fragment key={value.nome_evento}>
-              <VerticalTimelineElement
-                id={value.nome_evento}
-                className="vertical-timeline-element--work"
-                contentStyle={{ background: "#326475", color: "#fff" }}
-                date={value.data_inizio + "   -   " + value.data_fine}
-                iconStyle={{ background: "#326475", color: "#fff" }}
-                icon={
-                  <span
-                    key={value.nome_evento}
-                    className="material-symbols-outlined"
-                  >
-                    location_on
-                  </span>
-                }
-              >
-                <h3 className="vertical-timeline-element-title mb-3">
-                  {value.nome_evento}
-                </h3>
-                <h4 className="vertical-timeline-element-subtitle mb-4">
-                  {value.luogo}
-                </h4>
-                {value.descrizione !== "" && (
-                  <p className="font-italic">{value.descrizione}</p>
-                )}
-              </VerticalTimelineElement>
-            </Fragment>
-          ))}
-      </VerticalTimeline>
+      {events.some(isEventDateAfterToday) && (
+        <>
+          <h3 className="homepage-subtitle text-light text-center my-5">
+            Dove possiamo incontrarci
+          </h3>
+          <VerticalTimeline className="vertical-timeline-custom-line mt-2">
+            {events.filter(isEventDateAfterToday).map((value) => (
+              <Fragment key={value.nome_evento}>
+                <VerticalTimelineElement
+                  id={value.nome_evento}
+                  className="vertical-timeline-element--work"
+                  contentStyle={{ background: "#326475", color: "#fff" }}
+                  date={value.data_inizio + "   -   " + value.data_fine}
+                  iconStyle={{ background: "#326475", color: "#fff" }}
+                  icon={
+                    <span
+                      key={value.nome_evento}
+                      className="material-symbols-outlined"
+                    >
+                      location_on
+                    </span>
+                  }
+                >
+                  <h3 className="vertical-timeline-element-title mb-3">
+                    {value.nome_evento}
+                  </h3>
+                  <h4 className="vertical-timeline-element-subtitle mb-4">
+                    {value.luogo}
+                  </h4>
+                  {value.descrizione !== "" && (
+                    <p className="font-italic">{value.descrizione}</p>
+                  )}
+                </VerticalTimelineElement>
+              </Fragment>
+            ))}
+          </VerticalTimeline>
+        </>
+      )}
       <h3 className="homepage-subtitle text-light text-center my-5">
         Dove sono stato
       </h3>
-      <VerticalTimeline
-        className="vertical-timeline-custom-line mt-2 mb-5"
-      >
+      <VerticalTimeline className="vertical-timeline-custom-line mt-2 mb-5">
         {events
           .filter((value) => {
             const dateParts: string[] = value.data_fine.split("/");
