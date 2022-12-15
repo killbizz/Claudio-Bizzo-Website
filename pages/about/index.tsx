0a2f9bb05766a04fea28e08fbc44f49ac9f73a98
killbizz <gabriel.bizzo@hotmail.it>
2022-12-15 20:50:19 +0100

    added the main structure and style to the about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,18 +1,42 @@
-import Layout from '../../components/Layout';
+import Image from "next/image";
+import Layout from "../../components/Layout";
+import history from "../../data/about";
 
-interface AboutPageProps {
-    userId: string | null
-}
-
-const AboutPage = ({ userId }: AboutPageProps) => {
-
-  return(
-      <Layout title="Chi Sono | Claudio Bizzo">
-          <div className="mid">
-            <p>DAGHE</p>
+const AboutPage = () => {
+  return (
+    <Layout title="Chi Sono | Claudio Bizzo">
+      <div className="mid mid-background-color">
+        <div className="row mx-0">
+          <div className="col-lg-6 px-0 my-0">
+            <div className="row mx-0">
+              <div className="col-12 px-0 my-0 about-image">
+                <Image
+                  src="/about_main_img.jpg"
+                  id="aboutImg"
+                  alt="Claudio Bizzo about image"
+                  priority
+                  layout="fill"
+                  objectFit="cover"
+                  objectPosition={"0% 25%"}
+                />
+              </div>
+              <div className="col px-0 my-0 h-100 mid-background-color">
+                NOME-COGNOME, CONTATTI (email, indirizzo) + BUTTONS (contattami, esplora la galleria)
+              </div>
+            </div>
           </div>
-      </Layout>
+          <div className="col-lg-6 px-0 my-0">
+            <div className="title-container">
+              <h1 className="my-auto text-center">CHI SONO</h1>
+            </div>
+            <div className="about-history-container mid-dark-background-color py-2">
+              <div>{history}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
-}
+};
 
 export default AboutPage;
