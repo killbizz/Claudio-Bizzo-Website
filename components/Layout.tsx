21451b12f3be1389cb8ead69f6955501e02c9ee1
killbizz <gabriel.bizzo@hotmail.it>
2023-02-15 18:07:38 +0100

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,7 +22,7 @@ const Layout = ({ children, title, description }: Props) => {
   <div className="wrap">
     <Head>
       <title>{title}</title>
-      <meta name="description" content={description} />
+      <meta name="description" content={description} key={"desc"} />
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" type="image/x-icon" href='/favicon.ico' />
